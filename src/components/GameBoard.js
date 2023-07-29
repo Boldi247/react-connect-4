1f0a437fa52c0ccca7414b3fbfa428f948446a66
Boldi247 <kovacs.boldizsar0321@gmail.com>
2023-07-30 00:23:44 +0200

    creating coloring logic

--- src/components/GameBoard.js
+++ src/components/GameBoard.js
@@ -1,56 +1,68 @@
-import React from "react";
+import React, { useState } from "react";
 import GameCircle from "./GameCircle";
 import "../Game.css";
 
 const GameBoard = () => {
+    const [gameBoard, setGameBoard] = useState(Array(16).fill(0));
+
+    console.log(gameBoard);
+
+    const circleClicked = (id) => {
+        console.log("circle clicked: " + id);
+
+        gameBoard[id] = 1;
+        setGameBoard(gameBoard);
+        console.log(gameBoard);
+    }
+
     return (
         <div className="gameBoard">
-            <GameCircle id={1}>
+            <GameCircle id={1} className="player_1" onCircleClicked={circleClicked}>
 
             </GameCircle>
-            <GameCircle id={2}>
+            <GameCircle id={2} className="player_2" onCircleClicked={circleClicked}>
 
             </GameCircle>
-            <GameCircle id={3}>
+            <GameCircle id={3} onCircleClicked={circleClicked}>
 
             </GameCircle>
-            <GameCircle id={4}>
+            <GameCircle id={4} onCircleClicked={circleClicked}>
 
             </GameCircle>
-            <GameCircle id={5}>
+            <GameCircle id={5} onCircleClicked={circleClicked}>
 
             </GameCircle>
-            <GameCircle id={6}>
+            <GameCircle id={6} onCircleClicked={circleClicked}>
 
             </GameCircle>
-            <GameCircle id={7}>
+            <GameCircle id={7} onCircleClicked={circleClicked}>
 
             </GameCircle>
-            <GameCircle id={8}>
+            <GameCircle id={8} onCircleClicked={circleClicked}>
 
             </GameCircle>
-            <GameCircle id={9}>
+            <GameCircle id={9} onCircleClicked={circleClicked}>
 
             </GameCircle>
-            <GameCircle id={10}>
+            <GameCircle id={10} onCircleClicked={circleClicked}>
 
             </GameCircle>
-            <GameCircle id={11}>
+            <GameCircle id={11} onCircleClicked={circleClicked}>
 
             </GameCircle>
-            <GameCircle id={12}>
+            <GameCircle id={12} onCircleClicked={circleClicked}>
 
             </GameCircle>
-            <GameCircle id={13}>
+            <GameCircle id={13} onCircleClicked={circleClicked}>
 
             </GameCircle>
-            <GameCircle id={14}>
+            <GameCircle id={14} onCircleClicked={circleClicked}>
 
             </GameCircle>
-            <GameCircle id={15}>
+            <GameCircle id={15} onCircleClicked={circleClicked}>
 
             </GameCircle>
-            <GameCircle id={16}>
+            <GameCircle id={16} onCircleClicked={circleClicked}>
 
             </GameCircle>
         </div>
--- src/components/GameCircle.js
+++ src/components/GameCircle.js
@@ -1,19 +1,11 @@
 import React from 'react'
 import '../Game.css'
 
-const onClick = (event, id, color) => {
-    alert("on click" + id);
-}
 
-const GameCircle = ({ id, color, children }) => {
 
-    const style = {
-        backgroundColor: color,
-    }
-
-    console.log(id);
+const GameCircle = ({ id, children, className, onCircleClicked }) => {
     return (
-        <div className="gameCircle" style={id % 2 === 0 ? { backgroundColor: 'red' } : { backgroundColor: 'blue' }} onClick={(event) => onClick(event, id, color)}>
+        <div className={`gameCircle ${className}`} onClick={() => onCircleClicked(id)}>
             {children}
         </div>
     )
